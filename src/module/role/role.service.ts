c50afdb3aaa976817f753b633ae29975367d66dd
HaiTran <haitran23031999@gmail.com>
2023-04-04 08:29:27 +0700

    Tue 04/04/2023  8:29:27.53

--- src/module/role/role.service.ts
+++ src/module/role/role.service.ts
@@ -20,8 +20,8 @@ export class RoleService {
         const existedIds: Array<number> = existedRoles.map((e: role): number => e.id)
         const lackingIds: Array<number> = RoleIds.filter((e: number): boolean => !existedIds.includes(e))
         const lackingRoles = Object.entries(initRoles)
-          .filter((e: any): boolean => lackingIds.includes(e[1]))
-          .map((e) => {
+          .filter((e: [string, number]): boolean => lackingIds.includes(e[1]))
+          .map((e: [string, number]) => {
             return {
               id: e[1],
               name: e[0],
